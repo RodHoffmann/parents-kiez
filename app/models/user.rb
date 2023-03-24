bd134271a9678f98c3bbd55de50ed19add8bde6a
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-03-24 15:01:21 +0100

    Added the chatroom and message model, and created the references between them and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :chatrooms_as_user1, class_name: "Chatroom", foreign_key: :user1_id
+  has_many :chatrooms_as_user2, class_name: "Chatroom", foreign_key: :user2_id
 end
