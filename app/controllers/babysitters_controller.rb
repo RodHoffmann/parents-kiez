81ac01b28fd026a8826e0bbe2015c8babe691221
zain srio <zein.srio@gmail.com>
2023-03-24 11:21:43 +0100

    controller action,create, new, index and show aredone with the related view pages

--- app/controllers/babysitters_controller.rb
+++ app/controllers/babysitters_controller.rb
@@ -1,2 +1,35 @@
 class BabysittersController < ApplicationController
+  before_action :set_babysitter, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @babysitters = Babysitter.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @babysitter = Babysitter.new
+  end
+
+  def create
+    @babysitter = Babysitter.new(babysitter_params)
+    @babysitter.user = current_user
+    if @babysitter.save
+      redirect_to babysitters_path, notice: "You are now a babysitter!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_babysitter
+    @babysitter = Babysitter.find(params[:id])
+  end
+
+  def babysitter_params
+    params.require(:babysitter).permit(:first_name, :first_name,:age, :gender, :address, :cost_per_hour, :years_of_experience, :image)
+  end
 end
