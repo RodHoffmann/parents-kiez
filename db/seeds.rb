22f10c076960906515a20545e5caf92d815de39f
Rodrigo Hoffmann <116598092+RodHoffmann@users.noreply.github.com>
2023-03-29 15:26:57 +0200

    Update seeds.rb
    Removed 2 user seeds (There were already). Just kept the items of the new changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,72 +11,6 @@
 #   password: "password"
 # )
 
-# Item.create(
-#   name: "Example Item",
-#   description: "This is an example item.",
-#   age: "5-10 years",
-#   house: "Ravenclaw",
-#   available: true,
-#   user_id: User.last.id
-# )
-
-# Create users
-user1 = User.create!(
-  email: "user1@example.com",
-  password: "password",
-  first_name: "John",
-  last_name: "Doe",
-  address: "123 Main St"
-)
-
-user2 = User.create!(
-  email: "user2@example.com",
-  password: "password",
-  first_name: "Jane",
-  last_name: "Doe",
-  address: "456 Elm St"
-)
-
-# Create items
-item1 = user1.items.create!(
-  name: "Baby rattle",
-  description: "A colorful plastic rattle for babies",
-  age: "0-6 months",
-  house: "clean",
-  available: true,
-  user_id: User.last.id
-)
-p item1
-
-item2 = user2.items.create!(
-  name: "Toddler shoes",
-  description: "A pair of used toddler shoes in good condition",
-  age: "1-2 years",
-  house: "pet-free",
-  available: true,
-  user_id: User.last.id
-)
-p item2
-
-item3 = user1.items.create!(
-  name: "Baby clothes",
-  description: "A bag of gently used baby clothes",
-  age: "0-6 months",
-  house: "smoke-free",
-  available: true,
-  user_id: User.last.id
-)
-p item3
-
-item4 = user2.items.create!(
-  name: "Potty seat",
-  description: "A used potty seat in good condition",
-  age: "1-3 years",
-  house: "clean",
-  available: true,
-  user_id: User.last.id
-)
-p item4
 puts "Deleting Users..."
 
 User.destroy_all
@@ -139,6 +73,50 @@ p rodrigo
 users << rodrigo
 
 puts "Users creation completed successfuly"
+
+puts "Creating Items.."
+
+item1 = john.items.create!(
+  name: "Baby rattle",
+  description: "A colorful plastic rattle for babies",
+  age: "0-6 months",
+  house: "clean",
+  available: true,
+  user_id: User.last.id
+)
+p item1
+
+item2 = felix.items.create!(
+  name: "Toddler shoes",
+  description: "A pair of used toddler shoes in good condition",
+  age: "1-2 years",
+  house: "pet-free",
+  available: true,
+  user_id: User.last.id
+)
+p item2
+
+item3 = jane.items.create!(
+  name: "Baby clothes",
+  description: "A bag of gently used baby clothes",
+  age: "0-6 months",
+  house: "smoke-free",
+  available: true,
+  user_id: User.last.id
+)
+p item3
+
+item4 = norma.items.create!(
+  name: "Potty seat",
+  description: "A used potty seat in good condition",
+  age: "1-3 years",
+  house: "clean",
+  available: true,
+  user_id: User.last.id
+)
+p item4
+
+puts "Items created successfully"
 
 puts "Creating Chatrooms..."
 
