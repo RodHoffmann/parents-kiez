781a1023600891a456034840f27003da3ac1c0e3
Ahmad Shakib Omar <shakib.omar@hotmail.com>
2023-03-29 12:25:57 +0200

    The conflict fixed and the code merged completely.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :babysitter, dependent: :destroy
   has_many :items, dependent: :destroy
-  has_many :reservations, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-
-<<<<<<< HEAD
-  resources :items
-
-  root to: "pages#home"
-=======
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
+
+  resources :items
+  resources :babysitters
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
-
-  resources :babysitters
->>>>>>> master
 end
