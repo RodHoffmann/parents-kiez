48a42d14393d5ef1ecc524c595fbd41dc8a06959
zain srio <zein.srio@gmail.com>
2023-04-10 14:31:36 +0200

    added validation to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description_and_address_and_cost_and_date_and_category,
+                  against: %i[name description address cost date category],
+                  using: {
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+                  }
 end
