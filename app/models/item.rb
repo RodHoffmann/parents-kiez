856a23978251a6927808f3f68786e67f77327fee
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-04-20 15:55:10 +0200

    Styled Items Create Form

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
 
   acts_as_votable
 
-  AGES = ["less than 1 month old", "between 1 and 3 months old", "between 3 and 6 months old", "between 6 and 12 months old", "between 1 and 1,5 years old", "between 1 and 1,5 years old", "between 1,5 and 2 years old", "more than 2 years old"]
+  AGES = ["any age", "less than 1 month old", "between 1 and 3 months old", "between 3 and 6 months old", "between 6 and 12 months old", "between 1 and 1,5 years old", "between 1 and 1,5 years old", "between 1,5 and 2 years old", "more than 2 years old"]
   include PgSearch::Model
   pg_search_scope :search_by_name_and_description_and_age,
                   against: %i[name description age],
